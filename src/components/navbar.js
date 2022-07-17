04fe297b79bd4a7e3a592d85f4b475121d2aadf0
vinicius-hayden <vinihaydenss@gmail.com>
2022-07-17 20:40:24 -0300

    navbar component created

---
+++ src/components/navbar.js
@@ -0,0 +1,6 @@
+export const header = document.createElement('header');
+export const nav = document.createElement('nav');
+export const a = document.createElement('a');
+export const ul = document.createElement('ul');
+export const li = document.createElement('li')
+export const liA = document.createElement('a');
